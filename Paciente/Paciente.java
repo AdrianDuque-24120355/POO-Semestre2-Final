b49e78a2343e643a273b3237e66fc1c82f541793
AdrianDuque-24120355 <adrian.montufar81005@gmail.com>
2024-09-29 21:16:34 -0600

    Tarea 12 finalizada

---
+++ Paciente/Paciente.java
@@ -0,0 +1,57 @@
+package paciente;
+
+import java.time.LocalDate;
+
+public class Paciente {
+    public String id;
+    public String nombre;
+    public String apellidos;
+    public LocalDate FechaNacimiento;
+    private String telefono;
+    public String tipoSangre;
+    public char sexo;
+
+    public Paciente(String id, String nombre, String apellidos, LocalDate FechaNacimiento, String telefono, String tipoSangre, char sexo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.FechaNacimiento = FechaNacimiento;
+        this.telefono = telefono;
+        this.tipoSangre = tipoSangre;
+        this.sexo = sexo;
+    }
+    
+    public String MostrarDatos(){
+        String datos=String.format("Id: %s, nombre: %s, apellidos: %s, fecha de nacimiento: %s, telefono: %s, tipo de sangre: %s, sexo: %s",
+        id, nombre, apellidos, FechaNacimiento, telefono, tipoSangre, sexo);
+        return datos;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return FechaNacimiento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getTipoSangre() {
+        return tipoSangre;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+}
